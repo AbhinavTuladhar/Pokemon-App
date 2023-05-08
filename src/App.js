90c8b07c949b9438383837f001863bd8fa0dd0ff
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-08 11:47:42 +0545

    reworked urls

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path='' element={<WelcomePage />} />
-        <Route path='/gen-1' element={<MainPage idRange={[1, 151]} />} />
-        <Route path='/gen-2' element={<MainPage idRange={[152, 251]} />} />
-        <Route path='/gen-3' element={<MainPage idRange={[252, 386]} />} />
-        <Route path='/gen-4' element={<MainPage idRange={[387, 493]} />} />
-        <Route path='/gen-5' element={<MainPage idRange={[494, 649]} />} />
-        <Route path='/gen-6' element={<MainPage idRange={[650, 721]} />} />
+        <Route path='/pokemon/generation-1' element={<MainPage idRange={[1, 151]} />} />
+        <Route path='/pokemon/generation-2' element={<MainPage idRange={[152, 251]} />} />
+        <Route path='/pokemon/generation-3' element={<MainPage idRange={[252, 386]} />} />
+        <Route path='/pokemon/generation-4' element={<MainPage idRange={[387, 493]} />} />
+        <Route path='/pokemon/generation-5' element={<MainPage idRange={[494, 649]} />} />
+        <Route path='/pokemon/generation-6' element={<MainPage idRange={[650, 721]} />} />
         <Route path='/pokemon/:id' element={<PokemonDetail />} />
       </Routes>
     </>  
