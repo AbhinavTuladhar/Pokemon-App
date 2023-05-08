7ae2fde118c6b13cd727adda40ccd8fdb4b8945d
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-08 17:09:38 +0545

    implemented a basic ability component

---
+++ src/components/PokemonDetail/Abilities.jsx
@@ -0,0 +1,51 @@
+import { React, useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Abilities = ( { data } ) => {
+  // data is an array of ability-related objects.
+
+  const [abilityDetails, setAbilityDetails] = useState([])
+  const abilityNames = data.map(ability => {
+    return ability.ability.name
+  })
+  const abilityLinks = data.map(ability => {
+    return ability.ability.url
+  })
+  
+  const fetchDescriptions = async () => {
+    const responses = await Promise.all(abilityLinks.map(url => {
+      return axios.get(url)
+    }))
+    const newData = responses.map(response => response.data)
+    setAbilityDetails(() => {
+      return newData.map(entry => {
+        const englishData = entry.effect_entries.find(effectEntry => effectEntry.language.name === 'en');
+        return {
+          name: entry.name,
+          description: englishData.effect
+        };
+      });
+    });
+    
+  }
+
+  useEffect(() => {
+    fetchDescriptions()
+  }, [data])
+
+  if (!abilityDetails)
+    return
+
+  const abilityFinalResult = abilityDetails.map(ability => {
+    return (
+      <div>
+        {ability.name} = {ability.description}
+      </div>
+    )
+  })
+
+  return (
+    <div> {abilityFinalResult} </div>)
+}
+
+export default Abilities;
--- src/components/PokemonDetail/PokemonDetail.jsx
+++ src/components/PokemonDetail/PokemonDetail.jsx
@@ -1,117 +1,54 @@
-// import { React, useEffect, useState} from 'react'
-// import { useParams } from 'react-router-dom'
-// import axios from 'axios'
-
-// const PokemonDetail = () => {
-//   const { id } = useParams()
-//   const [data, setData] = useState([])
-//   const [loading, setLoading] = useState(true)
-//   let detailInformation = ''
-
-//   const fetchData = async () => {
-//     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-//     const responseData = await response.data
-//     setData(responseData)
-//     setLoading(false)
-//   }
-
-//   useEffect(() => {
-//     setLoading(true)
-//     fetchData()
-//   }, [])
-
-//   useEffect(() => {
-//     console.log(data)
-//   }, [data])
-
-//   useEffect(() => {
-//     const { 
-//       abilities,
-//       id: idNumber,
-//       name
-//     } = data;  
-//     const properName = name.charAt(0).toUpperCase() + name.slice(1);
-  
-//     abilities.forEach(ability => {
-//       console.log(ability.ability.name)
-//     })
-
-//     detailInformation = (
-//       <div className='text-4xl font-bold flex justify-center items-center'>
-//         #{idNumber}: {properName}
-//       </div>
-//     )
-//   }, [data])
-
-//   const loadingText = <h1> Loading... </h1>
-
-
-//   return (
-//     <>
-//       {loading ? loadingText : detailInformation}
-//     </>
-//   )
-// }
-
-// export default PokemonDetail;
-
 import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import Abilities from './Abilities';
 
 const PokemonDetail = () => {
   const { id } = useParams();
   const [pokemon, setPokemon] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [abilityData, setAbilityData] = useState([]);
 
   const fetchData = async () => {
-    setLoading(true);
+
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
     const responseData = await response.data;
     setPokemon(responseData);
-    setLoading(false);
   };
 
   const extractInformation = (data) => {
-    
-  }
+    const { abilities, id: idNumber, name } = data;
+    setAbilityData(abilities);
+  };
 
   useEffect(() => {
     fetchData();
   }, [id]);
 
-  const loadingScreen = (
-    <div className='text-white bg-gradient-to-br from-slate-600 to-slate-900 min-h-screen'>
-      Loading...
-    </div>
-  )
-  
+  useEffect(() => {
+    if (pokemon) {
+      extractInformation(pokemon);
+    }
+  }, [pokemon]);
+
   if (!pokemon) {
-    return loadingScreen
+    return (
+      <div className='text-white bg-gradient-to-br from-slate-600 to-slate-900 min-h-screen'>
+        Loading...
+      </div>
+    );
   }
-  
-  const { abilities, id: idNumber, name } = pokemon;
+
+  const { id: idNumber, name } = pokemon;
   const properName = name.charAt(0).toUpperCase() + name.slice(1);
-
-  const detailedInformation = (
-    <div className="text-4xl font-bold flex justify-center items-center">
-      #{idNumber}: {properName}
-    </div>
-  );
-
-  abilities.forEach((ability) => {
-    console.log(ability.ability.name);
-  });
 
   return (
     <div className='gap-x-2 gap-y-3 pt-4 text-white bg-gradient-to-br from-slate-600 to-slate-900 min-h-screen'>
-      {/* {loading && !pokemon ? loadingScreen : detailedInformation} */}
       <div className="text-4xl font-bold flex justify-center">
         #{idNumber}: {properName}
       </div>
-    </div>)
+      <Abilities data={abilityData} />
+    </div>
+  );
 };
 
 export default PokemonDetail;
-
-
