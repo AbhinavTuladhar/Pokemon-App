2f261e5d112993d5dfa7e39e744c4ccc62ee0fa2
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-11 17:46:30 +0545

    Implemented regional dex numbers list

--- src/components/PokemonDetail/PokeDexData.jsx
+++ src/components/PokemonDetail/PokeDexData.jsx
@@ -4,6 +4,9 @@ import TypeCard from '../TypeCard'
 const PokeDexData = ({ pokemonData }) => {
   const { id, types, genus, height, weight, abilities, pokedex_numbers } = pokemonData
 
+  console.log('Greetings from pokeDexdata')
+  console.log(pokedex_numbers)
+
   const formattedHeight = `${(height*0.1).toFixed(2)} m`
   const formattedWeight = `${(weight*0.1).toFixed(2)} kg`
 
@@ -15,7 +18,13 @@ const PokeDexData = ({ pokemonData }) => {
   })
 
   const typeDiv = typeNames.map(typeName => <TypeCard typeName={typeName} />)
+
   const abilityList = abilityNames.map(ability => <li> {ability} </li>)
+  const abilityListFinal = (
+    <ol className='list-inside list-decimal'>
+      {abilityList}
+    </ol>
+  )
 
   // Now dealing with the Pokedex numbers for each region.
   // Omit the national region number ssince it has already been covered.
@@ -24,11 +33,16 @@ const PokeDexData = ({ pokemonData }) => {
     const regionName = entry.pokedex.name
     return { number: entry.entry_number, region: regionName }
   })
-
-  const abilityListFinal = (
-    <ol className='list-inside list-decimal'>
-      {abilityList}
-    </ol>
+  
+  const regionNumberList = regionNumberValues?.map((number, index) => {
+    return (
+      <li key={index}> {number.number} ({number.region}) </li>
+    )
+  })
+  const regionNumberListFinal = (
+    <ul className='list-inside list-none'>
+      {regionNumberList}
+    </ul>
   )
 
   const tableData = [
@@ -37,11 +51,12 @@ const PokeDexData = ({ pokemonData }) => {
     { label: 'Species', value: genus },
     { label: 'Height', value: formattedHeight },
     { label: 'Weight', value: formattedWeight },
-    { label: 'Abilities', value: abilityListFinal }
+    { label: 'Abilities', value: abilityListFinal },
+    { label: 'Regional no.', value: regionNumberListFinal}
   ]
 
   const tableEntries = tableData.map(row => {
-    const spacing = row.label === 'Abilities' ? 'min-h-14' : 'h-12 max-h-24'
+    const spacing = row.label === 'Abilities' || row.label === 'Regional no.' ? 'min-h-14 max-h-screen' : 'h-12'
     return (
       <div className={`flex flex-row border-t-[1px] border-gray-200 py-2 ${spacing}`}>
         <div className='flex justify-end text-right items-center w-3/12'>
