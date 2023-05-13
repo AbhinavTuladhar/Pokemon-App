02b8901429033ae0b5218ed8ae68dcbea52b69dd
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-13 20:52:56 +0545

    Added a grouping feature to the dex entries.

--- src/components/PokemonDetail/PokeDexEntry.jsx
+++ src/components/PokemonDetail/PokeDexEntry.jsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
+// This is for grouping the name of the games if they have the same description for the Pokemon.
+const groupByDescription = data => {
+  return data?.reduce((acc, current) => {
+    // First it's checked whehter the description already exists in the accumulator array.
+    const index = acc.findIndex(item => item.description === current.description)
+    // if it does, then append the version name.
+    // Else, make a new entry in the accumulator array.
+    if (index !== -1) {
+      acc[index].versionName.push(current.versionName)
+    } else {
+      acc.push({ versionName: [current.versionName], description: current.description})
+    }
+    return acc
+  }, [])
+}
+
 const PokeDexEntry = ( { data }) => {
   if (!Array.isArray(data)) return null
 
@@ -19,18 +35,31 @@ const PokeDexEntry = ( { data }) => {
     }
   })
 
-  const entryRows = englishInfo.map((entry, i) => {
+  const englishInfoByDescription = groupByDescription(englishInfo)
+  console.log('From dex entry')
+  console.log(englishInfoByDescription)
+
+  // Now making a list for each version
+  const finalEntry = englishInfoByDescription.map(entry => {
+    const gameListItems = entry.versionName.map((version, index) => {
+      return <li key={index}> {version} </li>
+    })
+    const gameList = (<ul className='list-none list-inside'> {gameListItems} </ul>)
+    return {versionName: gameList, description: entry.description }
+  })
+
+  const entryRows = finalEntry.map((entry, i) => {
     return (
       <div className='flex flex-row border-t-[1px] border-gray-200 py-2'>
         <div className='flex justify-end items-center w-2/12 text-right'> {entry.versionName} </div>
-        <div className='flex justify-start pl-4 w-10/12'> {entry.description} </div>
+        <div className='flex justify-start items-center pl-4 w-10/12'> {entry.description} </div>
       </div>
     )
   })
 
   return (
     <>
-      <div className='font-bold text-3xl mb-10'> Pokédex Entry </div>
+      <div className='font-bold text-3xl mb-10'> Pokédex Entries </div>
       <div className='flex flex-col border-b-[1px] border-gray-200'>
         {entryRows}
       </div>
