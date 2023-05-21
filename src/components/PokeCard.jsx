553b763dd49da4152a7a46e6130af699a2daf676
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-21 20:02:35 +0545

    added hover effect and electric colour change

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -45,10 +45,10 @@ const PokeCard = ({ data }) => {
 
   // Now do some complicated shenanigans to use a one-step darker shade as the stopping colour for mono-type pokemon 
   const endingColour = secondType === undefined ? darkenColour(firstColour) : `to-${secondColour}`
-  const gradientStyle = `${startingColour} ${endingColour}`
+  const gradientStyle = `bg-gradient-to-tr from-10% to-90% ${startingColour} ${endingColour}`
 
   return (
-    <div className={`${gradientStyle} bg-gradient-to-tr rounded-xl flex flex-col justify-center items-center w-full smmd:w-2/12 sm:w-1/3 md:w-1/4 m-4 py-2`}>
+    <div className={`${gradientStyle} rounded-xl flex flex-col justify-center items-center w-full smmd:w-2/12 sm:w-1/3 md:w-1/4 m-4 py-2 hover:scale-105 hover:shadow-xl hover:shadow-blue-400 hover:drop-shadow-lg duration-200`}>
       <div className='font-bold'>
         #{id}
       </div>
