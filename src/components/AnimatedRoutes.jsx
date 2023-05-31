f9a4948b0b4d99188d9b77f689137421a471d34e
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-05-31 14:11:39 +0545

    fix: removed code splitting
    It caused exit transitions to not occur

--- src/components/AnimatedRoutes.jsx
+++ src/components/AnimatedRoutes.jsx
@@ -1,14 +1,14 @@
-import React, { lazy, Suspense } from 'react'
-import MainPage from '../pages/MainPage'
+import React from 'react'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-// Lazy loaded components
-const WelcomePage = lazy(() => import('../pages/WelcomePage'))
-const PokemonDetail = lazy(() => import('../pages/PokemonDetail'))
-const TypeDetail = lazy(() => import('./OtherDetails/TypeDetail'))
-const MoveDetail = lazy(() => import('../pages/MoveDetail'))
-const TypeListing = lazy(() => import('../pages/TypeListing'))
-const MoveListing = lazy(() => import('../pages/MoveListing'))
+
+import MainPage from '../pages/MainPage'
+import WelcomePage from '../pages/WelcomePage'
+import PokemonDetail from '../pages/PokemonDetail'
+import TypeDetail from './OtherDetails/TypeDetail'
+import MoveDetail from '../pages/MoveDetail'
+import TypeListing from '../pages/TypeListing'
+import MoveListing from '../pages/MoveListing'
 
 const AnimatedRoutes = () => {
   const location = useLocation()
@@ -30,17 +30,15 @@ const AnimatedRoutes = () => {
 
   return (
     <AnimatePresence mode='wait'>
-      <Suspense>
-        <Routes location={location} key={location.pathname}>
-          <Route path='' element={<WelcomePage />} />
-          <Route path='/types' element={<TypeListing />} />
-          <Route path='/moves' element={<MoveListing />} />
-          {generationRoutes}
-          <Route path='/pokemon/:id' element={<PokemonDetail />} />
-          <Route path='/types/:type' element={<TypeDetail />} />
-          <Route path='/moves/:id' element={<MoveDetail />} />
-        </Routes>
-      </Suspense>
+      <Routes location={location} key={location.pathname}>
+        <Route path='' element={<WelcomePage />} />
+        <Route path='/types' element={<TypeListing />} />
+        <Route path='/moves' element={<MoveListing />} />
+        {generationRoutes}
+        <Route path='/pokemon/:id' element={<PokemonDetail />} />
+        <Route path='/types/:type' element={<TypeDetail />} />
+        <Route path='/moves/:id' element={<MoveDetail />} />
+      </Routes>
     </AnimatePresence>
   )
 }
