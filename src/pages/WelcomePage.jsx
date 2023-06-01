863ce1cd95a253e25298c53ddf521bfa5d287051
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-06-01 17:43:49 +0545

    update: Welcome Page text

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -13,14 +13,15 @@ const WelcomePage = () => {
       initial={{opacity: 0}}
       animate={{opacity: 1}}
       exit={{opacity: 0}}
-      className='mx-2 flex flex-col justify-center items-center gap-y-4'
+      className='mx-2 pt-10 flex flex-col justify-center items-center gap-y-4'
     >
-      <div className="flex text-5xl justify-center font-bold mt-24 mx-auto">
+      <div className="flex text-5xl justify-center font-bold mx-auto">
         Welcome!
       </div>
       <div className='text-center'>
         This is a simple clone of the <a href='https://pokemondb.net/' className='text-blue-500'> Pokemon Database website. </a>
-        This was made entirely using ReactJS.
+        This was made using ReactJS, React Router, Tailwind CSS and some other libraries. <br />
+        See the package.json file in my GitHub repo linked below for details.
       </div>
       <div className='text-center'>
         You can view the (incredibly noob-ish) source code <a href='https://github.com/AbhinavTuladhar/Pokemon-App' className='text-blue-500'> here. </a>
