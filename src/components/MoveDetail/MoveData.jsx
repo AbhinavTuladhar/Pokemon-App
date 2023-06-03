2adb6d7c5f8069cb6f892a9f9834b772b74c950c
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-06-03 15:38:49 +0545

    feat: put image in move detail for damage class

--- src/components/MoveDetail/MoveData.jsx
+++ src/components/MoveDetail/MoveData.jsx
@@ -3,6 +3,21 @@ import TypeCard from '../TypeCard'
 import TableContainer from '../TableContainer'
 import SectionTitle from '../SectionTitle'
 import formatName from '../../utils/NameFormatting'
+import movePhysical from '../../images/move-physical.png'
+import moveSpecial from '../../images/move-special.png'
+import moveStatus from '../../images/move-status.png'
+
+// For damage class image.
+const returnMoveImage = damageClass => {
+  if (damageClass === 'physical')
+    return movePhysical
+  else if (damageClass === 'special')
+    return moveSpecial
+  else if (damageClass === 'status')
+    return moveStatus
+  else
+    return ''
+}
 
 const MoveData = ({ moveInfo }) => {
   const {
@@ -16,7 +31,14 @@ const MoveData = ({ moveInfo }) => {
   
   const rowData = [
     { header: 'Type', value: <TypeCard typeName={ moveType } /> },
-    { header: 'Category', value: formatName(damageClass) },
+    { 
+      header: 'Category', 
+      value: 
+        (<div className='flex flex-row gap-x-4 items-center'>
+          <img src={returnMoveImage(damageClass)} className='h-[20px] w-[30px]' />
+          <> {formatName(damageClass)} </>
+        </div>)
+    },
     { header: 'Power', value: power, },
     { header: 'Accuracy', value: accuracy, },
     { header: 'PP', value: <> { `${PP} (max. ${PP*1.6})` } </>, },
