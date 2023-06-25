2be57578c48bd8f06eec27c5a670314a7a2055df
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-06-25 11:53:24 +0545

    feat: add animation to type list

--- src/pages/TypeListing.jsx
+++ src/pages/TypeListing.jsx
@@ -2,6 +2,14 @@ import { React, useMemo } from 'react'
 import { motion } from 'framer-motion'
 import TypeCard from '../components/TypeCard'
 
+const typeVariant = {
+  initial: { y: "5rem", opacity: 0 },
+  animate: { 
+    y: 0, opacity: 1,
+    transition: { staggerChildren: 0.075, ease: "easeOut", duration: 0.3 },
+  },
+}
+
 const TypeListing = () => {
   const typeList = useMemo(() => {
     return [
@@ -9,7 +17,11 @@ const TypeListing = () => {
     ]
   }, [])
 
-  const typeCardList = typeList.map(type => <TypeCard typeName={type } />)
+  const typeCardList = typeList.map(type => (
+    <motion.div variants={typeVariant}>
+      <TypeCard typeName={type} />
+    </motion.div>
+  ))
 
   document.title = 'Pokémon types | Pokémon database'
 
@@ -21,12 +33,12 @@ const TypeListing = () => {
       exit={{ opacity: 0, transitionDuration: '0.5s' }}
       transition={{ ease: 'easeIn'}}
     >
-      <h1 className='flex text-center justify-center text-4xl mb-10 font-bold'> 
+      <motion.h1 className='flex text-center justify-center text-4xl mb-10 font-bold' variants={typeVariant}> 
         Type List
-      </h1>
-      <div className='flex flex-row flex-wrap gap-4 justify-center'>
+      </motion.h1>
+      <motion.div className='flex flex-row flex-wrap gap-4 justify-center' variants={typeVariant} initial='initial' animate='animate'>
         { typeCardList }
-      </div>
+      </motion.div>
     </motion.div>
   )
 }
