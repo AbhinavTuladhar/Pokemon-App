080be4847540ee2dda78b9eee17aaf3b19413eec
AbhinavTuladhar <84128922+AbhinavTuladhar@users.noreply.github.com>
2023-06-27 16:18:10 +0545

    feat: add basic sprite table

--- src/pages/PokemonDetail/SpriteTable.jsx
+++ src/pages/PokemonDetail/SpriteTable.jsx
@@ -13,9 +13,50 @@ const SpriteTable = ({ data }) => {
 
   console.log(properSpriteCollection)
 
+  // Vertical headers
+  const headers = [
+    { generation: 'Type', frontSprite: 'Normal', shinySprite: 'Shiny' }
+  ]
+
+  const tableColumns = [...headers, ...properSpriteCollection].map((row, index) => {
+    const { generation, frontSprite, shinySprite } = row
+    const normalImage = index === 0 ?
+      <span className='font-bold'> { frontSprite } </span>
+      :
+      <img src={frontSprite} alt={pokemonName} className='w-24 h-24' />
+
+    const shinyImage = index === 0 ?
+      <span className='font-bold'> { shinySprite } </span>
+      :
+      <img src={shinySprite} alt={pokemonName} className='w-24 h-24' />
+
+    // Cell data for each column.
+    const cellData = [
+      { key: 'Generation', value: <span className='font-bold'> { generation } </span> },
+      { key: 'Normal Sprite', value: normalImage },
+      { key: 'Shiny Sprite', value: shinyImage }
+    ]
+
+    // These cells are aligned vertically.
+    const tableCells = cellData.map(cell => (
+      <div className='flex border border-slate-400 items-center justify-center align-middle text-center h-32 w-32'>
+        { cell.value }
+      </div>
+    )) 
+
+    return (
+      <div className='flex flex-col min-h-32 min-w-96'>
+        { tableCells }
+      </div>
+    )
+  })
+
   return (
     <>
       <SectionTitle text={`${formatName(pokemonName)} sprites`} />
+        <div className='flex flex-row justify-center items-center'> 
+          { tableColumns }
+        </div>
     </>
   )
 }
