4c89d1c30b468abbb0b8c17571cd01e74de8334f
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2023-11-18 14:41:01 +0545

    refactor movelisting to usequeries

--- src/pages/MoveListing.jsx
+++ src/pages/MoveListing.jsx
@@ -1,9 +1,8 @@
 import { React, useState, useEffect, useMemo } from 'react'
-import { useQuery } from '@tanstack/react-query'
+import { useQueries } from '@tanstack/react-query'
 import { useLocation } from 'react-router-dom'
 import { motion } from 'framer-motion'
 import { NavLink } from 'react-router-dom'
-import axios from 'axios'
 import TableContainer from '../components/TableContainer'
 import TypeCard from '../components/TypeCard'
 import MoveListingSkeleton from '../components/MoveListingSkeleton'
@@ -13,6 +12,7 @@ import movePhysical from '../images/move-physical.png'
 import moveSpecial from '../images/move-special.png'
 import moveStatus from '../images/move-status.png'
 import '../index.css'
+import fetchData from '../utils/fetchData'
 
 // For damage class image.
 const returnMoveImage = damageClass => {
@@ -43,44 +43,45 @@ const MoveListing = () => {
   const urlList = useMemo(() => {
     let urls = []
     // default is 621 up to gen 6, 728 for gen 7.
-    for (let i = 1; i <= 728; i++) {
+    for (let i = 1; i <= 51; i++) {
       urls.push(`https://pokeapi.co/api/v2/move/${i}/`)
     }
     return urls
   }, [])
 
-  // For fetching data from a url.
-  const fetchData = async (url) => {
-    const response = await axios.get(url)
-    return response.data
-  }
-
-  // Perform a GET request on all the 'calculated' URLs.
-  const { data: moveData } = useQuery({
-    queryKey: ['moveData', urlList],
-    queryFn: () => Promise.all(urlList.map(fetchData)),
-    staleTime: Infinity,
-    cacheTime: Infinity
+  const { data: moveData, isFullyLoadedMoveData } = useQueries({
+    queries: urlList.map(url => {
+      return {
+        queryKey: ['move-url', url],
+        queryFn: () => fetchData(url),
+        staleTime: Infinity,
+        cacheTime: Infinity,
+        select: data => {
+          const { id, moveName, moveType, damageClass, power, accuracy, PP, shortEntry, effect_chance: effectChance, machines } = extractMoveInformation(data)
+          return { id, moveName, moveType, damageClass, power, accuracy, PP, shortEntry, effectChance, machines }
+        }
+      }
+    }),
+    combine: results => {
+      return {
+        data: results?.map(result => result?.data).sort((prev, curr) => prev?.moveName > curr?.moveName ? 1 : -1),
+        isLoading: results.some(result => result.isLoading),
+        isFullyLoadedMoveData: results.every(result => result.data !== undefined)
+      }
+    }
   })
 
   useEffect(() => {
-    if (!moveData)
+    if (!isFullyLoadedMoveData)
       return
-    const extracted = moveData.map(move => extractMoveInformation(move))
-    setMoveList(() => {
-      const extractedNew = extracted.map(move => {
-        const { id, moveName, moveType, damageClass, power, accuracy, PP, shortEntry, effect_chance: effectChance, machines } = move
-        return { id, moveName, moveType, damageClass, power, accuracy, PP, shortEntry, effectChance, machines }
-      })
-      // sorting alphabetically
-      return extractedNew.sort((prev, curr) => prev.moveName > curr.moveName ? 1 : -1)
-    })
-  }, [moveData])
+    setMoveList(moveData)
+  }, [moveData, isFullyLoadedMoveData])
 
   // Find the tm.
   useEffect(() => {
     if (!moveList)
       return
+
     const tmURLs = moveList?.map(move => {
       const ORASEntry = move?.machines.find(obj => obj.version_group.name === 'omega-ruby-alpha-sapphire')
       return {
@@ -89,18 +90,28 @@ const MoveListing = () => {
       }
     })
     setTMURLs(tmURLs)
-  }, [moveList])
+  }, [moveList, isFullyLoadedMoveData])
 
-  // Now query the urls.
-  const { data: TMResponse } = useQuery({
-    queryKey: ['TMList', TMURLs],
-    queryFn: () => {
-      const availableURLs = TMURLs?.filter(machine => machine.url !== undefined)
-      const TMURLList = availableURLs?.map(machine => machine.url)
-      return Promise.all(TMURLList.map(fetchData))
-    },
-    staleTime: Infinity,
-    cacheTime: Infinity
+  const availableURLs = TMURLs?.filter(machine => machine.url !== undefined)
+  const TMURLList = availableURLs?.map(machine => machine.url)
+
+  const { data: TMResponse, isFullyLoaded: isFullyLoadedTMResponse } = useQueries({
+    queries: TMURLList.map(url => {
+      return {
+        queryKey: ['tm-url', url],
+        queryFn: () => fetchData(url),
+        staleTime: Infinity,
+        cacheTime: Infinity,
+        enabled: isFullyLoadedMoveData
+      }
+    }),
+    combine: results => {
+      return {
+        data: results?.map(result => result?.data),
+        isLoading: results.some(result => result.isLoading),
+        isFullyLoadedMoveData: results.every(result => result.data !== undefined)
+      }
+    }
   })
 
   // Now make an array of objects - the object has two keys - 
@@ -110,23 +121,24 @@ const MoveListing = () => {
       return;
     }
 
-    const formattedData = TMResponse.map(move => {
+    const formattedData = TMResponse?.map(move => {
       // Reformat the TM number.
-      const TMNumber = move.item.name
-      const formattedTM = TMNumber.slice(0, 2).toUpperCase() + TMNumber.slice(2)
+      const TMNumber = move?.item?.name
+      const formattedTM = TMNumber?.slice(0, 2).toUpperCase() + TMNumber?.slice(2)
       return {
-        name: move.move.name,
+        name: move?.move?.name,
         machine: formattedTM
       }
     });
 
     setTMData(formattedData);
-  }, [TMResponse]);
+  }, [TMResponse, isFullyLoadedTMResponse]);
 
   // Now join TMdata and MOveList on the basis of the move name and set the result to moveListReady.
   useEffect(() => {
-    if (!moveList && !TMData)
+    if (!moveList && !TMData) {
       return
+    }
     const joinedData = moveList?.map(obj1 => {
       const obj2 = TMData?.find(obj => obj1.moveName === obj.name)
       return { ...obj1, ...obj2 }
@@ -161,13 +173,13 @@ const MoveListing = () => {
     // Provide a border on all sides and bold the text for the header.
     const headerStyle = index === 0 ? 'font-bold' : ''
     // Separate background colours for the header and odd-even rows.
-    let bgColour
+    let bgColourStyle
     if (index !== 0 && index % 2 === 0) {
-      bgColour = 'bg-gray-900'
+      bgColourStyle = 'bg-gray-900'
     } else if (index === 0) {
-      bgColour = 'bg-[#1a1a1a]'
+      bgColourStyle = 'bg-[#1a1a1a]'
     } else {
-      bgColour = ''
+      bgColourStyle = ''
     }
     // An image for the move type.
     const moveClassImage = returnMoveImage(damageClass)
@@ -218,7 +230,7 @@ const MoveListing = () => {
     const tableCells = tableCellData.map((cell, cellIndex) => {
       return (
         <div
-          className={`${cell.style} ${headerStyle} ${bgColour} border-gray-500 border-t table-cell h-12 align-middle p-2`}
+          className={`${cell.style} ${headerStyle} ${bgColourStyle} border-gray-500 border-t table-cell h-12 align-middle p-2`}
           key={cellIndex}
         >
           {cell.value}
@@ -244,12 +256,12 @@ const MoveListing = () => {
         <input
           className='text-black rounded-xl mx-4 mb-4 py-2 px-4 w-full lg:w-[20rem]' type='search'
           placeholder='Search for a move'
-          disabled={moveTableRows?.length < 2 && TMData.length === 0 ? true : false}
+          disabled={!isFullyLoadedMoveData && !isFullyLoadedTMResponse}
           onChange={handleChange}
         />
       </div>
       {/* // Checking if data is present */}
-      {(moveTableRows?.length < 2 && TMData.length === 0) ?
+      {(moveTableRows?.length < 2 || TMData.length === 0) ?
         <MoveListingSkeleton rowCount={20} />
         :
         <TableContainer child={moveTableRows} />
